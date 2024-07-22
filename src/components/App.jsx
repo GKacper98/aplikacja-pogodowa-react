0b02156eeb7b33c89f39c453c2bc8d385a02d973
GKacper98 <k.gleich98@gmail.com>
2024-07-22 11:41:19 +0200

    aplikacja pogodowa react

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './Form'
+import Result from './Result'
+
+function App() {
+
+  const [value,setValue] = useState("")
+  const [date, setDate] = useState("");
+  const [city, setCity] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
+  const [temp, setTemp] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [wind, setWind] = useState("");
+  const [err, setErr] = useState("");
+
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleCitySubmit = (e) => {
+    e.preventDefault();
+    console.log("potwierdzone");
+    
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=53ad542efa2753b468bb3d0c1ab24e95`;
+
+    fetch(API)
+    .then(response => {
+      if(response.ok){
+        return response
+      }
+      throw Error("Nie udao się")
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err)) 
+    
+  }
+
+
+  return (
+
+        <div className="App">
+        <Form 
+        value={value} setValue={setValue} 
+        change ={handleInputChange}
+        submit ={handleCitySubmit}
+        />
+        <Result />
+        </div>
+        
+  )
+}
+
+export default App
---
+++ src/components/Result.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState } from 'react'
+
+const Result = () => {
+  return (
+    <div>Miasto, pogoda...</div>
+  );
+}
+
+export default Result;
